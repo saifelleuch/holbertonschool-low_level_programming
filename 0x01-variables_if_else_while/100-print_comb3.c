c327fd986bc1e9f23569bcfb1034111f719b65bd
saifelleuch <sa2014if@gmail.com>
2021-10-04 03:05:22 -0700

    another one

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+
+	{
+		int i, j;
+		for (i = 48; i <= 57; i++)
+		
+		{
+			for (j = i+1 ; j <= 57; j++)
+
+			{ if ( i < j )
+				{
+			putchar(i);
+			putchar(j);
+				
+			if ( i == 56 && j == 57 )
+			
+			{
+			break;	
+			}
+				
+
+			putchar(',');
+			putchar(' ');
+				}
+			}
+		}
+		putchar('\n');	
+
+		return (0);
+
+	}
