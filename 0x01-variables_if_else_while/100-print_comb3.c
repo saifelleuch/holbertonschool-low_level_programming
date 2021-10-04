9ae34838b4a854991ac8ca437116928d994df330
saifelleuch <sa2014if@gmail.com>
2021-10-04 03:51:24 -0700

    another one

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,37 +6,28 @@
  * Return: Always 0 (Success)
  */
 
-	int main(void)
+int main(void)
 
-	{
-		int i, j;
+{
+int i, j;
 
-		for (i = 48; i <= 57; i++)
-
-		{
-			for (j = i + 1 ; j <= 57; j++)
-
-			{
-				if (i < j)
-
-					{
-						putchar(i);
-						putchar(j);
-
-				if (i == 56 && j == 57)
-
-							{
-								break;
-							}	
-
-
-							putchar(',');
-							putchar(' ');
-					}
-			}
-		}
-		putchar('\n');
-
-		return (0);
-
-	}
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1 ; j <= 57; j++)
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
