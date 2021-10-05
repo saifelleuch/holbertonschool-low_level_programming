1694188371c9f106252a9ef8bd05d26b8407c4f9
saifelleuch <sa2014if@gmail.com>
2021-10-05 14:11:13 -0700

    another one

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,12 @@
 
 int print_last_digit(int c)
 {
+	int a;
+
 	if (c <= 0)
-	c = -c;
-	_putchar((c % 10) + '0');
-	return (c % 10);
+	a = -1 * (c % 10);
+	else
+	a = c % 10;
+	_putchar(a + '0');
+	return (a);
 }
