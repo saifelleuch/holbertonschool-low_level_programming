4ffe026a2043df9db3dc0e7ad8be24933fc9f1a1
saifelleuch <sa2014if@gmail.com>
2021-10-05 15:54:19 -0700

    another one

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,19 +9,37 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, o;
 
 	for (i = 0; i < 10; i++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 
-			printf("%3d", i * j);
+		o = i * j;
+		if (o > 9)
+		{
+		_putchar((o / 10) + '0');
+		_putchar((o % 10) + '0');
+		}
+		else
+		{
+		_putchar(' ');
+		_putchar(o + '0');
+		}
+		if (j < 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
 
 		}
 
-	printf("\n");
+	_putchar('\n');
 
 	}
 
