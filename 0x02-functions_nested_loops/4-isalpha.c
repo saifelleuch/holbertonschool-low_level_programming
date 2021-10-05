7de899363e9864897c4eef3c92f353947b7249f1
saifelleuch <sa2014if@gmail.com>
2021-10-05 11:21:42 -0700

    another one

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: variable to check
+ * Return: 1 if c is lowercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 && c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
