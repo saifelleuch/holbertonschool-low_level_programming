44aeacffd797eaa7d9bf57ef16d0e13e956aadc6
saifelleuch <sa2014if@gmail.com>
2021-10-05 06:24:47 -0700

    another one

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -10,7 +10,7 @@
 void print_alphabet_x10(void)
 {
 
-int a = 1 , j;
+int a = 1, j;
 
 	while (a <= 10)
 
