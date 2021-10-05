6572ddd0e443859e1cb636c5fa17ff0bcd6f27e5
saifelleuch <sa2014if@gmail.com>
2021-10-05 06:22:21 -0700

    another one

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+int a = 1 , j;
+
+	while (a <= 10)
+
+	{
+		a++;
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+	}
+
+}
