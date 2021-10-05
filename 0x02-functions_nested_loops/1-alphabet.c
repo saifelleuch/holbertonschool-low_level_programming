7b1c7c7dc2bfdcef0cc17e5897c9814d8a35001e
saifelleuch <sa2014if@gmail.com>
2021-10-05 04:31:35 -0700

    another one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+	int a = 'a';
+
+	while(a <= 'z')
+
+	{
+	_putchar(a);
+	a++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
