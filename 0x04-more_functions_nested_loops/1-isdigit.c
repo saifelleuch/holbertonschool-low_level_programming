9665fc588e71bb95f8e84f32245abfa4cf5274fd
saifelleuch <sa2014if@gmail.com>
2021-10-07 03:13:41 -0700

    another one

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c : check variable
+ * Return: Always 0 (Success)
+ */
+
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
