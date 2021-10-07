92864c2e3628661bbc684f27f95c2be82e2d5737
saifelleuch <sa2014if@gmail.com>
2021-10-07 03:42:10 -0700

    another one

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @a : int
+ * @b : int
+ * @c : result of a multipled by b
+ * Return: c
+ */
+
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
