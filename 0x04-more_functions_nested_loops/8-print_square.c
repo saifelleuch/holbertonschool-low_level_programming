b480823b7a09ea91232154d28f44c25517663e43
saifelleuch <sa2014if@gmail.com>
2021-10-07 15:51:43 -0700

    another one

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * @size: variable to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int i = 0, j;
+
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
