41454d40467484140dd8e9a3e4bd38f9a359670c
saifelleuch <sa2014if@gmail.com>
2021-10-07 03:56:06 -0700

    another one

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
