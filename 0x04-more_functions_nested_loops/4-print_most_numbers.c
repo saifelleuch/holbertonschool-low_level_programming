e1781dbae0b5669e275bf5fdc99d1b21f3c8e492
saifelleuch <sa2014if@gmail.com>
2021-10-07 04:16:30 -0700

    another one

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+			i++;
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
