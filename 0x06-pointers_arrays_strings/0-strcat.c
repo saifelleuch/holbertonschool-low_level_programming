572515cd3d94b4a853e285091f9ee37e9eeae609
saifelleuch <sa2014if@gmail.com>
2021-10-13 06:37:58 -0700

    another one

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - Entry point
+ * @src: pointer
+ * @dest: pointer
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	j = i;
+	k = 0;
+	while (src[k] != '\0')
+	{
+		dest[j] = src[k];
+		k++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
