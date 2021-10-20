154e8ca5bcf3f61b0632fe014bee8b43e90e9226
saifelleuch <sa2014if@gmail.com>
2021-10-20 15:08:36 -0700

    another one

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - function that fills memory with a constant byte.
+ * @x: int
+ * @y: int
+ * Return: Always 0 (Success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
