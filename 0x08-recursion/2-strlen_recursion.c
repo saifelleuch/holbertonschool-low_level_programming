118938468a862a30f0dd41b05292008096a17b68
saifelleuch <sa2014if@gmail.com>
2021-10-20 14:07:51 -0700

    another one

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that fills memory with a constant byte.
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (s[0] == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+
+}
