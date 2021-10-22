8a5e07e5302eb58d4d89260859d24d9eed8ebbb7
saifelleuch <sa2014if@gmail.com>
2021-10-22 03:15:32 -0700

    another one

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, mult;
+
+	for (i = 1; i < argc - 1; i++)
+	{
+		mult = atoi(argv[i]) * atoi(argv[i + 1]);
+		printf("%d\n", mult);
+	}
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
