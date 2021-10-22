03ceeaaa2f5f0c9a81e0cecfa26b881fd1222160
saifelleuch <sa2014if@gmail.com>
2021-10-22 04:06:09 -0700

    another one

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
  * main - function
@@ -10,11 +9,28 @@
  * Return: always 0
  */
 
-
 int main(int argc, char **argv)
 {
 
-	printf("%s\n", argv[argc - 1]);
+	int i, sum;
+
+	for (i = 1; i < argc - 1; i++)
+	{
+		if ( argv[i] is not digit)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum = atoi(argv[i]) + atoi(argv[i + 1]);
+			printf("%d\n", sum);
+		}
+	
+	}
+
+	if (argc < 2)
+		printf("0\n");
 
 	return (0);
 
