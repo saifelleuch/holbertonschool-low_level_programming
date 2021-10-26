48a2dc7bfe5c13a927d83b60476e26d598b384b8
saifelleuch <sa2014if@gmail.com>
2021-10-26 05:13:09 -0700

    another one

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,8 +17,10 @@ char *str_concat(char *s1, char *s2)
 	char *target;
 	unsigned int i, j;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	target = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 
