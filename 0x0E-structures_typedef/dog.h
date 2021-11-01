2f36027a922972254d1e257b3000b9208a54e7b8
saifelleuch <sa2014if@gmail.com>
2021-11-01 12:25:16 -0700

    another one

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
