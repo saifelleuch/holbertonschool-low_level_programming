777817b3d8b0ec9917ea1cdba9d7c42db20ddd1c
saifelleuch <sa2014if@gmail.com>
2021-11-02 03:41:59 -0700

    another one

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * *new_dog - function that print dog informations
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Always 0 (Success)
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	char *name_copy, *owner_copy;
+
+	dog_t *new_dog;
+
+	*name_copy = strdup(name);
+	*owner_copy = strdup(owner);
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+
+}
