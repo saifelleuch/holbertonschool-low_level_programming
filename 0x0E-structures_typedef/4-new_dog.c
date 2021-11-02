26ffb748b92c4a1b3ad7f22bdd8beef8421e2714
saifelleuch <sa2014if@gmail.com>
2021-11-02 03:50:59 -0700

    another one

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
