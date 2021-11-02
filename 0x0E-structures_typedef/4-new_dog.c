4ed56142d4909aaa6c608208edc0f799eb234e8c
saifelleuch <sa2014if@gmail.com>
2021-11-02 03:49:01 -0700

    another one

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,8 +18,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_t *new_dog;
 
-	*name_copy = strdup(name);
-	*owner_copy = strdup(owner);
+	name_copy = strdup(name);
+	*name_copy = *name;
+	owner_copy = strdup(owner);
+	*owner_copy = *owner;
 
 	new_dog = malloc(sizeof(dog_t));
 
