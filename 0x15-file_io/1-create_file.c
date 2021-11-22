fb9ed83e04a382403c3d4f5de705db9076b00202
saifelleuch <sa2014if@gmail.com>
2021-11-22 14:34:10 -0800

    another one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 		wr = write(fd, text_content, strlen(text_content));
 	close(fd);
 	if (wr == -1)
