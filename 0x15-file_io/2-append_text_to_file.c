3c74f41846e6c8fa9a1f4cdbaaf6e286ba9dd56d
saifelleuch <sa2014if@gmail.com>
2021-11-23 02:17:22 -0800

    another one

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,9 +18,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	wr = write(fd, text_content, strlen(text_content));
 	if (text_content == NULL && filename != NULL)
 		return (1);
+	wr = write(fd, text_content, strlen(text_content));
+
 	if (wr == -1)
 		return (-1);
 	close(fd);
