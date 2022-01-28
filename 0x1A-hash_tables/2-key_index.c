e3c20cfd0a50d70c00da7510d37f907d5a417f2e
saifelleuch <sa2014if@gmail.com>
2022-01-28 14:40:21 -0800

    another one

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,6 +10,8 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size)
+	unsigned long int index;
 
+	index = hash_djb2(key) % size;
+	return (index);
 }
