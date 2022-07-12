91297c835dd2142fb23f4344bc85c74799560b27
saifelleuch <sa2014if@gmail.com>
2022-07-12 02:12:26 +0100

    another one

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,12 +10,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
